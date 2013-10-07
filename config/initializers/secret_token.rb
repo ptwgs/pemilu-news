ea7233dcec4b71aacfde978404fe292889655208
William Notowidagdo <wnotowidagdo@gmail.com>
2013-10-07 13:22:10 +0700

    Added Rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PemiluNews::Application.config.secret_token = '966c66f6562bc45a4ce67ceb39f17901145a042c67c2b0a6e236848532befb2ca5baffdb2c1be37baf2064981eba190731fa4c9debde6e6075a7dc91279c1cc6'
